4f78be8c36812ae1a184d4866a267afab212d698
Hideman85 <factionacraft@gmail.com>
2020-08-25 17:50:40 +0200

    Fix missing imports

--- packages/graphql-dynamodb-transformer/src/ResourceFactoryOverride.ts
+++ packages/graphql-dynamodb-transformer/src/ResourceFactoryOverride.ts
@@ -1,4 +1,6 @@
 import {ResourceFactory} from './resources'
+import { ResourceConstants } from 'graphql-transformer-common';
+import {Fn, IntrinsicFunction} from 'cloudform-types'
 
 class ResourceFactoryOverride extends ResourceFactory {
   protected dynamoDBTableName(typeName: string): IntrinsicFunction {
